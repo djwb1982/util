ac3eb0013f70b69632f29905dace8c7074d7a0d3
djwb1982 <djwb1982@163.com>
2017-10-23 17:43:27 +0800

    add stringhandle & stringtool

--- src/Agile/Utils/StringTool.php
+++ src/Agile/Utils/StringTool.php
@@ -122,4 +122,97 @@ class StringTool
         }
         return $data;
     }
+
+    /**
+     * 生成随机码
+     *
+     * @param int $length
+     * @param bool|false $intmode
+     * @return string
+     */
+    static function random($length, $intmode = false)
+    {
+        $hash = '';
+        $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz';
+        $intmode and $chars = "0123456789";
+        $max = strlen($chars) - 1;
+        PHP_VERSION < '4.2.0' && mt_srand(( double )microtime() * 1000000);
+        for ($i = 0; $i < $length; $i++) {
+            $hash .= $chars [mt_rand(0, $max)];
+        }
+        return $hash;
+    }
+
+    //防止跨站攻击
+    static public function removeXss($val)
+    {
+        $val = preg_replace('/([\x00-\x08][\x0b-\x0c][\x0e-\x20])/', '', $val);
+
+        $search = 'abcdefghijklmnopqrstuvwxyz';
+        $search.= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $search.= '1234567890!@#$%^&*()';
+        $search.= '~`";:?+/={}[]-_|\'\\';
+
+        for ($i = 0; $i < strlen($search); $i++) {
+            $val = preg_replace('/(&#[x|X]0{0,8}'.dechex(ord($search[$i])).';?)/i', $search[$i], $val);
+            $val = preg_replace('/(&#0{0,8}'.ord($search[$i]).';?)/', $search[$i], $val);
+        }
+
+        $ra1 = array('javascript', 'vbscript', 'expression', 'applet', 'meta','blink', 'link',  'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound');
+        $ra2 = array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint',
+            'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged',
+            'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange',
+            'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave',
+            'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize',
+            'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload');
+        $ra = array_merge($ra1, $ra2);
+
+        $found = true;
+        while ($found == true) {
+            $val_before = $val;
+            for ($i = 0; $i < sizeof($ra); $i++) {
+                $pattern = '/';
+                for ($j = 0; $j < strlen($ra[$i]); $j++) {
+                    if ($j > 0) {
+                        $pattern .= '(';
+                        $pattern .= '(&#[x|X]0{0,8}([9][a][b]);?)?';
+                        $pattern .= '|(&#0{0,8}([9][10][13]);?)?';
+                        $pattern .= ')?';
+                    }
+                    $pattern .= $ra[$i][$j];
+                }
+                $pattern .= '/i';
+                $replacement = substr($ra[$i], 0, 2).'<x>'.substr($ra[$i], 2);
+                $val = preg_replace($pattern, $replacement, $val);
+                if ($val_before == $val) {
+                    $found = false;
+                }
+            }
+        }
+        return $val;
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed截取字符串
+     */
+    static  function cc_msubstr($str, $length, $start = 0, $charset = "utf-8", $suffix = true)
+    {
+        if (function_exists("mb_substr")) {
+            return mb_substr($str, $start, $length, $charset);
+        } elseif (function_exists('iconv_substr')) {
+            return iconv_substr($str, $start, $length, $charset);
+        }
+        $re['utf-8'] = "/[/x01-/x7f]|[/xc2-/xdf][/x80-/xbf]|[/xe0-/xef][/x80-/xbf]{2}|[/xf0-/xff][/x80-/xbf]{3}/";
+        $re['gb2312'] = "/[/x01-/x7f]|[/xb0-/xf7][/xa0-/xfe]/";
+        $re['gbk'] = "/[/x01-/x7f]|[/x81-/xfe][/x40-/xfe]/";
+        $re['big5'] = "/[/x01-/x7f]|[/x81-/xfe]([/x40-/x7e]|/xa1-/xfe])/";
+        preg_match_all($re[$charset], $str, $match);
+        $slice = join("", array_slice($match[0], $start, $length));
+        if ($suffix) {
+            return $slice . "..";
+        } else {
+            return $slice;
+        }
+    }
 }
